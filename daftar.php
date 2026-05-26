caf67472ef143a962932a4cc6847f045477fedfd
unknown <daffayuzakie@gmail.com>
2026-05-26 09:41:40 +0700

    first commit

---
+++ daftar.php
@@ -0,0 +1,31 @@
+<?php
+include 'koneksi.php';
+
+if (isset($_POST['submit'])){
+    $u = $_POST['username'];
+    $e = $_POST['email'];
+    $p = $_POST['password'];
+
+    $q = "INSERT INTO tb_users(username,email,password)VALUES('$u','$e','$p')";
+    $hasil = mysqli_query($conn, $q);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="">username</label><br>
+        <input type="text" name="username"><br>
+        <label for="">email</label><br>
+        <input type="email" name="email"><br>
+        <label for="">password</label><br>
+        <input type="password" name="password"><br>
+        <button name="submit">submit</button>
+    </form>
+</body>
+</html>
